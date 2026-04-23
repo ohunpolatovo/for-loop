e806328172259ffee5d4337de3736ca6f9a96852
olloberdi <ohunpolatov.o@gmail.com>
2026-04-23 07:34:38 +0500

    k va n soni berilgan k sonini n marta chiqaruvchi programma tuzildi

---
+++ main.js
@@ -0,0 +1,18 @@
+// For1. k va n butun sonlari berilgan (n > 0). k sonini n marta chiqaruvchi programma tuzilsin.
+// let k = +prompt("k sonini kiriting");
+// let n = +prompt("n sonini kiriting");
+// for (let i = 0; i < n; i++) {
+//   console.log(k);
+// }
+//============================================================================
+// For2. Bir kg konfetning narxi berilgan (haqiqiy son). 0.1, 0.2, …, 0.9, 1 kg konfetni narxini chiqaruvchi programma tuzilsin.
+
+// For5. A va B butun soni berilgan (A < B). A va B sonlari orasidagi barcha butun sonlarni chiqaruvchi programma tuzilsin. Bunda har bir son o'zining qiymaticha chiqarilsin. Ya'ni 3 soni 3 marta chiqariladi.
+
+// For6. Sonning bo’luvchilarini topish;
+
+// tub sonni topish
+
+// daftar orqasidagi kabi karra jadvalini chiqarib kelish
+
+//2 x 2 = 4
