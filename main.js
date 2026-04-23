9c8689d49d66ab9e827dbac91ae1067fd6bd7a5e
olloberdi <ohunpolatov.o@gmail.com>
2026-04-23 07:54:34 +0500

    1 kg konfetning narhi berilgan  biz uni 0.1 dan 1 kg oraligidagi konfetning narhini chiqaruvchi programma tuzdik

--- main.js
+++ main.js
@@ -6,7 +6,12 @@
 // }
 //============================================================================
 // For2. Bir kg konfetning narxi berilgan (haqiqiy son). 0.1, 0.2, …, 0.9, 1 kg konfetni narxini chiqaruvchi programma tuzilsin.
-
+// let narx = +prompt("1 kg konfetning narxini kiriting");
+// for (let i = 1; i <= 10; i++) {
+//   let kg = i / 10;
+//   console.log(narx * kg);
+// }
+//======================================================================
 // For5. A va B butun soni berilgan (A < B). A va B sonlari orasidagi barcha butun sonlarni chiqaruvchi programma tuzilsin. Bunda har bir son o'zining qiymaticha chiqarilsin. Ya'ni 3 soni 3 marta chiqariladi.
 
 // For6. Sonning bo’luvchilarini topish;
